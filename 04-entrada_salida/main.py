fefc945823d202343f92dd2aea12645c52b9cd1a
Alexis Solis <alexsr2590@gmail.com>
2023-08-13 16:29:48 -0600

    Entrada y salida de datos

---
+++ 04-entrada_salida/main.py
@@ -0,0 +1,7 @@
+# Entrada de datos
+name = input("Escribe tu nombre: ")
+age = (input("Cúal es tu edad?: "))
+
+# Salida de datos
+print("Bienvenido al curso de Python "+ name + " tienes " + age +" años.")
+print(f"En China tendrías {int(age) + 1} años")
